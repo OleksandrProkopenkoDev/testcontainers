fdd6b84515b9af1e77015db650b792a6a0dc4d2c
Oleksandr Prokopenko <oleksandr.prokopenko@student.itmont.de>
2024-01-06 19:13:39 +0200

    init

---
+++ src/test/java/ua/spro/tcdemo/TestContainersDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.spro.tcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestContainersDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
